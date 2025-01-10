77d709b07405ab347b222ef8e9913d772dbb549a
sohrab09 <sohrab.cse9@gmail.com>
2025-01-10 19:14:33 +0600

    hello and variabel

---
+++ First Program/variable.go
@@ -0,0 +1,14 @@
+// declare a variable with type
+package main
+
+import "fmt"
+
+func main() {
+	var x int
+	x = 10
+	fmt.Println(x)
+}
+
+// Run this code with the following command:
+// go run variable.go
+// The output will be:
